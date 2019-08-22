6705107e43a2436750eb818cebdc3f125b92c147
jessebettridge <jessebett92@gmail.com>
2019-08-21 23:18:37 -0600

    inital commit

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const Dragon = require("./dragon.js");
+
+const fooey = new Dragon({ birthdate: new Date(), nickname: "fooey" });
+
+const baloo = new Dragon({ birthdate: new Date(), nickname: "baloo" });
+
+console.log("fooey", fooey);
+console.log("baloo", baloo);
